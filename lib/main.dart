e08dd22b355bff12fa6f7c7a4705fc2d22965c2b
Abhishek <12213078@nitkkr.ac.in>
2023-10-07 18:45:41 +0530

    Step-3

--- lib/main.dart
+++ lib/main.dart
@@ -5,13 +5,16 @@ import 'package:mynotes/firebase_options.dart';
 import 'package:mynotes/views/login_view.dart';
 import 'package:mynotes/views/register_view.dart';
 import 'package:mynotes/views/verify_email_view.dart';
+import 'dart:developer' as devtools show log;
 
 
 void main() {
  
   WidgetsFlutterBinding.ensureInitialized(); 
+  devtools.log("jgjhbhj");
   runApp(MaterialApp(
       title: 'Flutter Demo',
+
       theme: ThemeData(
         primarySwatch: Colors.blue,
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
@@ -42,7 +45,7 @@ class HomePage extends StatelessWidget {
               // print(user);
               if(user!=null){
                 if(user.emailVerified){
-                  return const Text("email is verified");
+                  return const NotesView();
                 }else{
                   return const VerifyEmailView();
                 }
@@ -60,3 +63,74 @@ class HomePage extends StatelessWidget {
   }
 }
 
+class NotesView extends StatefulWidget {
+  const NotesView({super.key});
+
+  @override
+  State<NotesView> createState() => _NotesViewState();
+}
+enum MenuAction {logout}
+
+class _NotesViewState extends State<NotesView> {
+  @override
+  Widget build(BuildContext context) {
+    return  Scaffold(
+      appBar: AppBar(
+        title: const Text("Notes"),
+        backgroundColor: Colors.blueAccent,
+        actions: [
+          PopupMenuButton<MenuAction>(
+            onSelected: (value)async{
+              switch(value){
+                
+                case MenuAction.logout:
+                  final shouldLogout=await showLogOutDialog(context);
+                  if(shouldLogout){
+                    await FirebaseAuth.instance.signOut();
+                    if (!context.mounted) return;
+                    Navigator.of(context).pushNamedAndRemoveUntil('/login/', (route) => false);
+                  }
+                  // devtools.log(shouldLogout.toString());
+                  break;
+                default:
+                devtools.log("adfa");
+              }
+          },
+          itemBuilder: (context){
+            return const [
+           PopupMenuItem<MenuAction>(
+              value: MenuAction.logout,
+              child: Text("sign out"),
+            )
+            ];
+          },)
+        ],),
+      body: Container(padding:const EdgeInsets.all(12.0),
+      margin: const EdgeInsets.all(12.0),
+      height: 50,
+      decoration:BoxDecoration(border: Border.all(width:2,color:const Color.fromARGB(255, 26, 173, 23))),
+      
+      child: const Center(child:  Text("done"))),
+    );
+  }
+}
+
+Future<bool>showLogOutDialog(BuildContext context){
+  return showDialog(
+    context:context,
+    builder:(context){
+      return AlertDialog(
+        title: const Text("Sign Out"),
+        content: const Text("Are you sure you want to sign out?"),
+        actions: [
+          TextButton(onPressed: (){
+            Navigator.of(context).pop(false);
+          }, child: const Text("Cancel")),
+          TextButton(onPressed: (){
+            Navigator.of(context).pop(true);
+          }, child: const Text("Log Out")),
+        ],
+      );
+    }
+  ).then((value)=>value??false);
+}
